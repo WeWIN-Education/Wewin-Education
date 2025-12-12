7079f4a4d85d51041c036c27b824941f6df498a1
WeWIN IT <wewineduca@gmail.com>
2025-12-12 11:10:19 +0700

    Update bug in deploy

--- frontend/app/(management)/class/[id]/page.tsx
+++ frontend/app/(management)/class/[id]/page.tsx
@@ -220,6 +220,7 @@ export default function ClassDetailPage() {
 
           {/* 🔹 Pagination */}
           <Pagination
+            text="Students"
             currentPage={currentPage}
             totalPages={totalPages}
             startIndex={startIndex}
