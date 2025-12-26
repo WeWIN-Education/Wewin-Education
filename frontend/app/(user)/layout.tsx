367a158ad3f933ac080bf9524ee876aeaf994147
Thinh IT <wewineduca@gmail.com>
2025-12-26 11:05:14 +0700

    fix layout

--- frontend/app/(user)/layout.tsx
+++ frontend/app/(user)/layout.tsx
@@ -1,24 +1,20 @@
-// Layout cho user - KHÔNG yêu cầu đăng nhập
-export default function UserLayout({
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/authOptions";
+
+export default async function ManagementLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-<<<<<<< HEAD
   const session = await getServerSession(authOptions);
 
   if (!session) redirect("/login");
-  console.log(session);
 
-  const isAdmin = allowedEmails.includes(session.user?.email || "");
-  if (isAdmin) redirect("/");
+  const roles = session.user?.roles ?? [];
 
-  return <div className="min-h-screen overflow-visible">{children}</div>;
-=======
-  return (
-    <div className="min-h-screen overflow-visible">
-      {children}
-    </div>
-  );
->>>>>>> 372a23f4882285f56033ac5b3dc630385811a0ad
+  const isAdmin = roles.includes("ADMIN");
+  if (!isAdmin) redirect("/login"); 
+
+  return <>{children}</>;
 }
